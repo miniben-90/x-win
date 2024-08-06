cc310929fa4270019280984135cee7b0f9299845
hocine BENKHADRA <miniben90@hotmail.com>
2024-08-06 11:48:10 +0200

    fix(ci-rs.yml): Wrong script used for ci-rs.yml

--- .github/docker-ubuntu-rust-install.sh
+++ .github/docker-ubuntu-rust-install.sh
@@ -1,22 +1,12 @@
 #!bin/bash
 
-Xvfb :0 &
-
-sleep 1
-
-gpg-agent --daemon
-
-sleep 1
-
-xfce4-session &
-
-sleep 1
+target=${1:-''}
 
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 
 sudo apt install musl-tools -y
 
-rustup target add $CARGO_TARGET_ENV
+rustup target add $target
 
 curl -L -o /tmp/firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64"
 
@@ -25,7 +15,3 @@ sudo tar xjf /tmp/firefox.tar.bz2 -C /opt/
 /opt/firefox/firefox --safe-mode https://github.com/ &
 
 sleep 1
-
-cd /work
-
-cargo test --target $CARGO_TARGET_ENV
