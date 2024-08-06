fd1a266851c162ed870caa1bf06d7275a3dfee55
hocine BENKHADRA <miniben90@hotmail.com>
2024-08-06 10:43:04 +0200

    fix(ci-rs.yml,docker-ubuntu-rust-install): install only rust for docker

---
+++ .github/docker-ubuntu-rust-install.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+target=${1:-''}
+
+curl https://sh.rustup.rs -sSf | bash -s -- -y
+
+sudo apt install musl-tools -y
+
+rustup target add $target
+
+curl -L -o /tmp/firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64"
+
+sudo tar xjf /tmp/firefox.tar.bz2 -C /opt/
+
+/opt/firefox/firefox --safe-mode https://github.com/ &
+
+sleep 1
